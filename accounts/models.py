3cf22755ccba9f1b55cae8ca1c867c53a52f2793
kweku-xvi <nksarps@gmail.com>
2024-04-29 18:30:00 +0000

    add model to save user information

---
+++ accounts/models.py
@@ -0,0 +1,65 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,password:None,**extra_fields):
+        if not email:
+            raise ValueError(_("The email is Required"))
+        email = self.normalize_email(email=email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("SuperUser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("SuperUser must have is_superuser=True"))
+
+        return self.create_user(email,password,**extra_fields)
+
+
+class User(AbstractUser):
+    ROLE = [
+        ('User', 'User'),
+        ('Event Manager', 'Event Manager')
+    ]
+
+    id = models.CharField(max_length=8, primary_key=True, unique=True)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    role = models.CharField(max_length=30, choices=ROLE, default='User')
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    
+    def __str__(self):
+        return self.username
+
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = str(uuid.uuid4())[:8]
+        super().save(*args, **kwargs)
+
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    
+    
