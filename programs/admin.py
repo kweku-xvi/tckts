0afcb00b18156511d61d117c5865003f5dd50abb
kweku-xvi <nksarps@gmail.com>
2024-05-22 07:16:05 +0000

    added events to admin panel

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,10 @@
+from .models import Event
 from django.contrib import admin
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_id', 'title', 'description', 'date', 'start_time', 'location')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(Event, EventAdmin)
