f4f93876db7604319b2536f75096a4622beda84d
kweku-xvi <nksarps@gmail.com>
2024-05-28 15:26:56 +0000

    added url endpoint for ticket purchases

--- tickets/urls.py
+++ tickets/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<str:ticket_type_id>/update', views.update_ticket_type_info_view, name='update_ticket_type'),
     path('<str:ticket_type_id>/delete', views.delete_ticket_type_view, name='delete_ticket_type'),
     path('<str:event_id>', views.get_event_ticket_types_view, name='event_ticket_types_view'),
+    path('<str:ticket_type_id>/buy', views.purchase_ticket_view, name='purchase_ticket')
 ]
