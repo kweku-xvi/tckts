48d735719ffc0e4e66df7887a9a8119e20f7db67
kweku-xvi <nksarps@gmail.com>
2024-05-28 15:25:25 +0000

    added admin panel for ticket purchases

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,4 +1,4 @@
-from .models import TicketType
+from .models import TicketType, TicketPurchase
 from django.contrib import admin
 
 
@@ -6,4 +6,10 @@ class TicketTypeAdmin(admin.ModelAdmin):
     list_display = ('ticket_type_id', 'name', 'event', 'price')
 
 
+class TicketPurchaseAdmin(admin.ModelAdmin):
+    list_display = ('purchase_id', 'user', 'event', 'quantity', 'purchased_at')
+    readonly_fields = ('purchase_date', 'purchased_at')
+
+
 admin.site.register(TicketType, TicketTypeAdmin)
+admin.site.register(TicketPurchase, TicketPurchaseAdmin)
